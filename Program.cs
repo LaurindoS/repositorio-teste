f68f4eab14c6201786ed3c66284daa4390bf3668
Samuel Laurindo <samuelprivate200@gmail.com>
2021-08-26 13:54:29 -0300

    Teste com cores

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace repositorio_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Digite seu nome: ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
